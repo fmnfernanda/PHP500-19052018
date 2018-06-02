126085e751b6d9504edc1484ca85eccc9333bf68
Fernanda Nunes <fmnfernanda@gmail.com>
2018-06-02 17:49:51 -0300

    aula03

---
+++ aula03/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Bem Vindo</title>
+</head>
+
+<body>
+<p><a href="login.php">Login</a></p>
+<h1>Pagina dos Visitantes</h1>
+</body>
+</html>	
---
+++ aula03/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Login</title>
+</head>
+
+<body>
+<p><a href="index.php">Inicio</a></p>
+<form method="POST">
+	<p>Email:<input type="email" name="email"></p>
+	<p>Senha:<input type="password" name="senha"></p>
+	<p><input type="submit" value="Entrar"></p>
+</form>
+</body>
+</html>
+
+<?php 
+
+if (!empty($_POST)) {
+	if($_POST)	
+}
+?>
